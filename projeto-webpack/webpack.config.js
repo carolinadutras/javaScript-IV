66fff7b91d385bc40ea855d888b286400e45e33f
carolinadutras <carolina.dutras@gmail.com>
2019-11-08 16:44:41 -0300

    projeto usando webpack para instrodução de react

---
+++ projeto-webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                // 'style-loader',
+                'sass-loader'
+            ]
+        }]
+    }
+}
